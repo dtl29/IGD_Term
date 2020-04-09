aabe62cfd2f50e1dff8427b1bf11dd15a20d47b3
Daniel Louis <44950643+dtl29@users.noreply.github.com>
2020-04-09 13:28:57 -0400

    Adding The Game
    First commit of the game

---
+++ IGD_Term/Assets/FPS_CON/Scripts/PlayerFPS.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFPS : MonoBehaviour
+{
+    private int health = 100;
+    public GameObject rightHand;
+    private bool notSwinging = true;
+    private int SwingCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (notSwinging)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                notSwinging = false;
+            }
+        }
+        else
+        { 
+            Swing();
+        }
+    }
+
+    //mutator to change the players health 
+    void ChangeHealth(int num, bool increase)
+    {
+        if(increase)
+        {
+            health += num;
+        }
+        else
+        {
+            health -= num;
+            if(health <= 0)
+            {
+                Debug.Log("Start death sequnce");
+            }
+        }
+    }
+
+    void Swing()
+    {
+        if (SwingCount < 50)
+        {
+            Debug.Log("Attacking");
+            SwingCount++;
+        }
+        else
+        {
+            notSwinging = true;
+            SwingCount = 0;
+        }
+    }
+}
