e891560565e7cf2c85d669128e1592ae24bb4803
Daniel Louis <44950643+dtl29@users.noreply.github.com>
2020-04-10 11:14:27 -0400

    Fixing Rotation
    Need to push to master so I dont lose what I did I think

---
+++ IGD_Term/Assets/Script/DoorCheck.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorCheck : MonoBehaviour
+{
+    private GameObject Mommy;
+    private RoomsController RC;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Door")
+        {
+            //GetComponent<script name>.name of variable <-- to access variables from other scripts attached to something 
+            Mommy = this.transform.parent.gameObject;
+            Mommy.GetComponent<RoomsController>().ShouldSpawn = false;
+            Destroy(other);
+            Destroy(this);
+        }
+    }
+}
