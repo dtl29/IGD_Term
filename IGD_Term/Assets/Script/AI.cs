c18718817afe5b9c223f8f8de18fe4647b389ac3
mejv31 <mev31@zips.uakron.edu>
2020-04-25 15:47:09 -0400

    "AI follows player"

---
+++ IGD_Term/Assets/Script/AI.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    //public float chaseSpeed = 10f;
+    public bool attacked = false;
+    //public Transform Player;
+    public GameObject GamePlayer;
+
+    //Mesh cube;
+    //Rigidbody cube;
+    //private Vector3 directionOfPlayer;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        //cube = this.GetComponent<Rigidbody>();//= this.GetComponent<MeshFilter>().mesh;
+        //this.GetComponent<NavMeshAgent>();//.SetDestination(GamePlayer.transform.position);
+        //agent = gameObject.GetComponent<NavMeshAgent>();
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        else
+        {
+            agent = gameObject.GetComponent<NavMeshAgent>();
+            agent.SetDestination(GamePlayer.transform.position);
+            if (attacked)
+            {
+              //this.transform.position = Vector3.forward - directionOfPlayer.position;
+            }
+            /*else
+            {
+              this.transform.position += Vector3.right * Time.deltaTime;
+            }*/
+        }
+    }
+
+    void OnTriggerEnter (Collider Other)
+    {
+        if (Other.tag == "GamePlayer")
+        {
+            attacked = true;
+        }
+    }
+}
