ba6bf128cd35ada05b6ff53eae0feced64857fea
Daniel Louis <44950643+dtl29@users.noreply.github.com>
2020-04-28 12:37:55 -0400

    Fixing

--- IGD_Term/Assets/Script/Arrows.cs
+++ IGD_Term/Assets/Script/Arrows.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Arrows : MonoBehaviour
 {
     public int damage;
-    public GameObject player;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
